b4cc425a8be19fc1796f27261c7409dd6d95e5ed
Alan Christian <alanchristiancreator@hotmail.com>
2022-02-25 15:43:50 -0300

    feat: Add Feedback & setting a Proxy vitejs

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -18,7 +18,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
     const data = await response.json()
 
     setFeedback(data)
@@ -33,9 +33,19 @@ export const FeedbackProvider = ({ children }) => {
   }
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback)
+    })
+
+    const data = await response.json()
+    
+    // newFeedback.id = uuidv4()
+    setFeedback([data, ...feedback])
   }
 
   // Edit feedback
